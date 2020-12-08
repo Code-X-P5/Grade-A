875438f64606db85bda984e7711d1c6232ad8e50
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-08 02:02:11 +0200

    basic register functions

--- Front-End/src/components/Register.js
+++ Front-End/src/components/Register.js
@@ -1,30 +1,69 @@
-import React from 'react'
+import React,{useState} from 'react'
 import logo from './pics/logo.png'
+import axios from 'axios'
 
 const Register = () => {
+
+const [userName,setuserName]=useState('')
+const [email,setemail]=useState('')
+const [password,setpassword]=useState('')
+const [confPassword,setconfPassword]=useState('')
+const [phone,setphone]=useState('')
+const [adress,setadress]=useState('')
+const [role,setrole]=useState('')
+
+const userHandler=(e)=>{
+    setuserName(e.target.value)
+}
+const emailHandler=(e)=>{
+    setemail(e.target.value)
+}
+const passHandler=(e)=>{
+    setpassword(e.target.value)
+}
+const confPassHandler=(e)=>{
+    setconfPassword(e.target.value)
+}
+const phoneHandler=(e)=>{
+    setphone(e.target.value)
+}
+const adressHandler=(e)=>{
+    setadress(e.target.value)
+}
+const roleHandler=()=>{
+    setrole("instructor")
+}
+const onSignUp=(userName,adress,email,password,phone)=>{
+    axios.post("/register/:role",{userName,adress,email,password,phone})
+}
+
     return (
         <div className="registerCont">
             <div className="register">
                 <div className="registerLogo">
-                    <img src={logo}></img>
+                    <img src={logo} alt=""></img>
                     <p>Sign Up as student or Instructor</p>
                 </div>
                 <div className="s-border"></div>
                 <div className="inputs">
-                    <input placeholder="User Name" name="text" required/>
+                    <input placeholder="User Name" name="text" onChange={userHandler} required/>
 
-                    <input placeholder="Email" name="email" required/>
+                    <input placeholder="Email" name="email" onChange={emailHandler} required/>
 
-                    <input placeholder="Password" name="password" type="password" required/>
+                    <input placeholder="Password" name="password" type="password" onChange={passHandler} required/>
 
-                    <input placeholder="Confirm Password" name="password" type="password" required/>
+                    <input placeholder="Confirm Password" name="password" type="password" onChange={confPassHandler} required/>
 
-                    <input placeholder="Phone Number" name="number" type="number" required/>
+                    <input placeholder="Phone Number" name="number" type="number" onChange={phoneHandler} required/>
 
-                    <input placeholder="Adress" name="text" required/>
+                    <input placeholder="Adress" name="text" onChange={adressHandler} required/>
                 </div>
+                <div className="check">
+                            <input type="checkbox" name="" onClick={roleHandler} ></input>
+                            <label > Register as a Instructor</label>
+                        </div>
                 <div className="signup">
-                    <button>Sign Up</button>
+                    <button onClick={onSignUp}>Sign Up</button>
                     <p>By signing up, you agree to our Terms , Data Policy and Cookies Policy . </p>
                 </div>
             
