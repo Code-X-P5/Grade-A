a3b143f201eb00b0f516e0fc2de0a1b2a4a6ea22
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2020-12-09 03:19:46 +0200

    Finished login style

--- Front-End/src/components/Login.js
+++ Front-End/src/components/Login.js
@@ -7,9 +7,24 @@ const Login = () => {
             <div className="login">
                 <div className="loginLogo">
                     <img src={logo} alt=""></img>
+                    <p>Log In as student or Instructor</p>
                     <div className="l-border"></div>
+                    
+                </div>
+                <div className="inputs">
+
+                <input placeholder="Email" name="email" required/>
+
+                <input placeholder="Password" name="password" type="password" required/>
+                </div>
+                <div className="signup">
+                    <button >Login</button>
+                    <p>Login to enroll in courses and more . </p>
                 </div>
             </div>
+            <div className="not-sign">
+                <p>Don't have an account?</p><a href="http://localhost:3000/register">Sign up</a>
+            </div>  
         </div>
     )
 }
